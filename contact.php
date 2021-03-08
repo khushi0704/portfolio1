93b10eadd4a795105595615f197db83494588f74
Khushi Jain <khushi.jain0704@gmail.com>
2021-03-08 22:03:30 +0530

    updatedd

--- contact.php
+++ contact.php
@@ -1,19 +1,25 @@
 <?php 
-if(isset($_POST['submit'])){
-    $name = $_Post['name'];
-    $visitor_email = $_Post['email'];
-    $msg = $_Post['msg'];
+echo var_export($_POST);
+if(sizeof($_POST) > 0){
+    /* $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $msg = $_POST['msg'];
 
     $email_from = 'khushi.jain0704@gmail.com';
     $email_subject = "New Form Submission";
-    $email_body = "User Name : $name.\n".
-    "User Email: $visitor_email.\n".
-    "User Message: $msg.\n";
+    $email_body = "User Name : $name.\r\n".
+    "User Email: $visitor_email.\r\n".
+    "User Message: $msg.\r\n";
 
     $to = "khushi.jain0704@gmail.com";
-    $headers = "From: $email_from \r\n";
-    $headers = "Reply-To:$visitor_email \r\n";
+    $headers = "From: $email_from \r\r\n";
+    $headers = "Reply-To:$visitor_email \r\r\n";
     mail($to,$email_subject,$email_body,$headers);
-    header("Location: index.html");
+    header("Location: index.html"); */
+    $file = fopen("data.txt", "a");
+    fwrite($file, "Name: {$_POST["name"]}\r\n");
+    fwrite($file, "E-mail: {$_POST["email"]}\r\n");
+    fwrite($file, "Message: {$_POST["msg"]}\r\n\r\n");
+    fclose($file);
 }
 ?>
